da2d8e4df91506b71697cb1427ffcb1f73b86259
themeowcoin <meowcoin2023@gmail.com>
2023-05-08 19:54:08 -0300

    responsive

--- js/header.js
+++ js/header.js
@@ -6,14 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
 function setHeader() {
   let header = document.querySelector('header');
   header.innerHTML = `
-  <a href="/" class="header__logo">
+  <a href="/" class="head__logo">
   <img src="/img/logo-desktop.png" alt="logo">
+  <img src="/img/logo.png" alt="logo" class="head__logo-mobile">
   </a>
-  <nav class="header__nav">
-    <a href="https://twitter.com/the_meowcoin" target="_blank" class="header__item">TWITTER</a>
-    <a href="https://t.me/+Ei4KAwBwcPtmNzVh" target="_blank" class="header__item">TELEGRAM</a>
-    <a href="/" target="_blank" class="header__item">TOKENOMICS</a>
-    <a href="/nft" class="header__item">NFTs</a>
+  <nav class="head__nav">
+    <a href="https://twitter.com/the_meowcoin" target="_blank" class="head__item">TWITTER</a>
+    <a href="https://twitter.com/the_meowcoin" target="_blank" class="head__item mob"><img src="/img/twitter.png" alt="twitter"></a>
+    <a href="https://t.me/+Ei4KAwBwcPtmNzVh" target="_blank" class="head__item">TELEGRAM</a>
+    <a href="https://t.me/+Ei4KAwBwcPtmNzVh" target="_blank" class="head__item mob"><img src="/img/telegram.png" alt="telegram"></a>
+    <a href="https://github.com/themeowcoin" target="_blank" class="head__item">GITHUB</a>
+    <a href="https://github.com/themeowcoin" target="_blank" class="head__item mob"><img src="/img/github.png" alt="github"></a>
+    <a href="/" target="_blank" class="head__item">MEOWNOMICS</a>
+    <a href="/" target="_blank" class="head__item mob"><img src="/img/tokenomics.png" alt="meownomics"></a>
+    <a href="/nft" class="head__item">NFTs</a>
+    <a href="/nft" class="head__item mob"><img src="/img/nft.png" alt="nft"></a>
   </nav>
   `
   document.body.insertBefore(header, document.body.firstChild);
